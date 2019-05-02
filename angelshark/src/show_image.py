9e69b16c664f3619ce60c550bab9cffd8fef0101
Huseyin ERDEM <rootofarch@gmail.com>
2019-05-02 09:05:36 +0300

    added image shower

---
+++ angelshark/src/show_image.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TwistStamped
+from mavros_msgs.msg import Altitude, PositionTarget, State
+from cv_bridge import CvBridge, CvBridgeError
+import time
+import cv2
+
+class image_shower(object):
+    def __init__(self):
+        
+        self.cvbridge = CvBridge()
+        self.camera_subscriber = rospy.Subscriber('/angelshark/camera/image_raw', Image, self.camera_callback, queue_size=1)
+        
+        self.cam_data = None
+
+        self.rate = rospy.Rate(10)
+        
+        self.rate.sleep()
+
+
+    def camera_callback(self, msg):
+        try:
+            self.cam_data = self.cvbridge.imgmsg_to_cv2(msg, "mono8")
+        except CvBridgeError as e:
+            print("camera err:",e)
+
+    def show_img(self):
+        while not rospy.is_shutdown():
+            cv2.imshow("camera",self.cam_data)
+            
+            k = cv2.waitKey(1)
+            if k == 27:
+                break
+            
+            self.rate.sleep()
+
+def main():
+    rospy.init_node("angelshark_img_show",disable_signals=True)
+
+    camera = image_shower()
+    try:
+        camera.show_img()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
