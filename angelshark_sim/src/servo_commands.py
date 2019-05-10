752c4411571d425a1ff0b9140fff854eb659eb75
Huseyin ERDEM <rootofarch@gmail.com>
2019-05-10 23:33:24 +0300

    modified ackermann topic name

--- angelshark_sim/src/servo_commands.py
+++ angelshark_sim/src/servo_commands.py
@@ -33,7 +33,7 @@ def servo_commands():
 
     rospy.init_node('servo_commands', anonymous=True)
 
-    rospy.Subscriber("ackermann_cmd", AckermannDriveStamped, set_throttle_steer)
+    rospy.Subscriber("angelshark/ackermann_cmd", AckermannDriveStamped, set_throttle_steer)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
