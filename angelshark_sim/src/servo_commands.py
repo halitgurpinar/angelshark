ab9da444246600d2bf04e066dce3f45e99b35f11
Huseyin ERDEM <rootofarch@gmail.com>
2019-05-12 16:07:26 +0300

    added real ackermann steering to the front wheels

--- angelshark_sim/src/servo_commands.py
+++ angelshark_sim/src/servo_commands.py
@@ -4,7 +4,11 @@ from std_msgs.msg import Bool
 from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 from ackermann_msgs.msg import AckermannDriveStamped
+import math
 
+car_length = 144    #the distance between front and rear wheel in cm
+car_width = 110     #the distance between two front wheel in cm
+half_of_car_width = car_width/2
 flag_move = 0
 
 def set_throttle_steer(data):
@@ -21,13 +25,24 @@ def set_throttle_steer(data):
 
     throttle = data.drive.speed/0.1
     steer = data.drive.steering_angle
+    if steer != 0:
+        R = car_length / math.tan(steer)
+        right_steer = math.atan(car_length / (R - half_of_car_width))
+        left_steer  = math.atan(car_length / (R + half_of_car_width))
+        #rospy.loginfo('\x1b[1M\r'
+                      #'\033[34;1mSteer: \033[32;1m%0.2f, '
+                      #'\033[34;1mSteering right: \033[32;1m%0.2f rad\033[0m, '
+                      #'\033[34;1mSteering left: \033[32;1m%0.2f rad\033[0m',
+                      #steer, right_steer, left_steer)
+    else:
+        right_steer = left_steer = 0
 
     pub_vel_left_rear_wheel.publish(throttle)
     #pub_vel_right_rear_wheel.publish(throttle)
     #pub_vel_left_front_wheel.publish(throttle)
     #pub_vel_right_front_wheel.publish(throttle)
-    pub_pos_left_steering_hinge.publish(steer)
-    pub_pos_right_steering_hinge.publish(steer)
+    pub_pos_left_steering_hinge.publish(left_steer)
+    pub_pos_right_steering_hinge.publish(right_steer)
 
 def servo_commands():
 
